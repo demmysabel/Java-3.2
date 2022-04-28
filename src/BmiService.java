8b4032e2b610123900e90aac9d3121c47eb72e12
demmysabel <ekprokhorenko@gmail.com>
2022-04-28 15:16:03 +0300

    Code reformatted

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public float calculate (int weight, float height) {
-        float bmi = weight / (height*height);
+    public float calculate(int weight, float height) {
+        float bmi = weight / (height * height);
         return bmi;
     }
 
